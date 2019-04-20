4ef219bcfeb797bab8b7abd944f3c489dee78fcd
William Neil <36243469+rwneil@users.noreply.github.com>
2019-04-20 11:37:34 -0700

    Create AutoBoat_Post_MyTable.py
    First working version

---
+++ AutoBoat_Post_MyTable.py
@@ -0,0 +1,35 @@
+# importing the requests library
+import requests
+import datetime
+
+# defining the api-endpoint
+API_ENDPOINT = "https://qo8k2n8335.execute-api.us-west-1.amazonaws.com/Production/"
+
+# your API key here
+API_KEY = "XXXXXXXXXXXXXXXXX"
+
+# Generate Unique Key
+source = 'AutoBoat001'
+currdatetime = str(datetime.datetime.now())
+UniqueID = source + "|" + currdatetime
+
+MessageType = 'Testing'
+
+# data to be sent to api
+data = {
+    "ID" : UniqueID,
+    "MessageType" : MessageType,
+    "DateTime" : "04/19/2019 16:37",
+
+    "Lon" : "200",
+    "Lat" : "100"
+ }
+
+print(data)
+
+# sending post request and saving response as response object
+r = requests.post(url = API_ENDPOINT, json = data)
+
+# extracting response text
+pastebin_url = r.text
+print("The pastebin URL is:%s"%pastebin_url)
